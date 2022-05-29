00119cbae5e73ce9290a4eead9fa57cc671e5bee
Pablo Ocampo <pablojocampogo@gmail.com>
2022-05-29 18:08:59 -0400

    feat: add logs to indexer service

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/pokt-foundation/pocket-go v0.9.4
 	github.com/pokt-foundation/pocket-indexer-lib v0.1.1
+	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
 )
 
@@ -14,4 +15,5 @@ require (
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/lib/pq v1.10.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 )
--- service/main.go
+++ service/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"sync"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	indexerlib "github.com/pokt-foundation/pocket-indexer-lib"
 	postgresdriver "github.com/pokt-foundation/pocket-indexer-lib/postgres-driver"
 	"github.com/pokt-foundation/pocket-indexer-services/pkg/environment"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -21,6 +23,8 @@ var (
 	indexingProcesses sync.WaitGroup
 	semaphoreLimiter  *semaphore.Weighted
 
+	log = logrus.New()
+
 	clientTimeout    = environment.GetInt64("CLIENT_TIMEOUT", 60000)
 	clientRetries    = environment.GetInt64("CLIENT_RETRIES", 3)
 	serviceRetries   = environment.GetInt64("SERVICE_RETRIES", 3)
@@ -28,6 +32,11 @@ var (
 	reqInterval      = environment.GetInt64("REQUEST_INTERVAL", 5000)
 	connectionString = environment.GetString("CONNECTION_STRING", "")
 )
+
+func init() {
+	// log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&logrus.JSONFormatter{})
+}
 
 // indexer interface of needed functions for indexing
 type indexer interface {
@@ -69,6 +78,30 @@ type service struct {
 	retries          int64
 	concurrency      int64
 	reqInterval      time.Duration
+	mainNode         string
+	fallbackNode     string
+}
+
+func (s *service) logErrorWithFields(message string, height int, err error) {
+	fields := logrus.Fields{
+		"main_node":     s.mainNode,
+		"fallback_node": s.fallbackNode,
+		"err":           err.Error(),
+	}
+
+	if height >= 0 {
+		fields["height"] = height
+	}
+
+	log.WithFields(fields).Error(fmt.Sprintf("%s with error: %s", message, err.Error()))
+}
+
+func (s *service) logInfoWithFields(message string, height int) {
+	log.WithFields(logrus.Fields{
+		"main_node":     s.mainNode,
+		"fallback_node": s.fallbackNode,
+		"height":        height,
+	}).Info(fmt.Sprintf("%s with height: %d", message, height))
 }
 
 func (s *service) start() error {
@@ -182,8 +215,15 @@ func (s *service) indexBlock(height int) {
 
 	err := s.indexBlockWithRetries(height, s.indexer)
 	if err != nil {
-		s.indexBlockWithRetries(height, s.fallbackIndexer)
+		s.logErrorWithFields("Index block with main node failed", height, err)
+
+		err = s.indexBlockWithRetries(height, s.fallbackIndexer)
+		if err != nil {
+			s.logErrorWithFields("Index block with fallback node failed", height, err)
+		}
 	}
+
+	s.logInfoWithFields("Block indexed succesfully", height)
 }
 
 func (s *service) indexBlockWithRetries(height int, indexer indexer) error {
@@ -211,8 +251,15 @@ func (s *service) indexBlockTransactions(height int) {
 
 	err := s.indexBlockTransactionsWithRetries(height, s.indexer)
 	if err != nil {
-		s.indexBlockTransactionsWithRetries(height, s.fallbackIndexer)
+		s.logErrorWithFields("Index block with main node failed", height, err)
+
+		err = s.indexBlockTransactionsWithRetries(height, s.fallbackIndexer)
+		if err != nil {
+			s.logErrorWithFields("Index block with fallback node failed", height, err)
+		}
 	}
+
+	s.logInfoWithFields("Block transactions indexed succesfully", height)
 }
 
 func (s *service) indexBlockTransactionsWithRetries(height int, indexer indexer) error {
@@ -310,6 +357,8 @@ func setupService() (*service, error) {
 		retries:          serviceRetries,
 		concurrency:      concurrency,
 		reqInterval:      time.Duration(reqInterval) * time.Millisecond,
+		mainNode:         mainNode,
+		fallbackNode:     fallbackNode,
 	}
 
 	err = service.setOptionalParams(fromHeight, toHeight)
@@ -323,11 +372,13 @@ func setupService() (*service, error) {
 func main() {
 	service, err := setupService()
 	if err != nil {
-		return
+		log.WithError(err).Error(fmt.Sprintf("Setup service failed with error: %s", err.Error()))
 	}
 
 	err = service.start()
 	if err != nil {
-		return
+		service.logErrorWithFields("Start service failed", -1, err)
 	}
+
+	log.Info("Execution finished succesfully")
 }
