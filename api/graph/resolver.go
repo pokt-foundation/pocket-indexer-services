5bdcae28f8853bf54fa75f0007b0df3b86c7f977
Pablo Ocampo <pablojocampogo@gmail.com>
2022-06-07 18:13:22 -0400

    feat: add missing queries for graphQL API

--- api/graph/resolver.go
+++ api/graph/resolver.go
@@ -1,18 +1,24 @@
 package graph
 
-import indexerlib "github.com/pokt-foundation/pocket-indexer-lib"
+import (
+	indexerlib "github.com/pokt-foundation/pocket-indexer-lib"
+	postgresdriver "github.com/pokt-foundation/pocket-indexer-lib/postgres-driver"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-// driver interface of needed functions for the db driver
-type driver interface {
-	ReadBlocks() ([]*indexerlib.Block, error)
-	ReadTransactions() ([]*indexerlib.Transaction, error)
+// reader interface of needed functions for the db reader
+type reader interface {
+	ReadTransactions(options *postgresdriver.ReadTransactionsOptions) ([]*indexerlib.Transaction, error)
+	ReadTransactionsByAddress(address string, options *postgresdriver.ReadTransactionsByAddressOptions) ([]*indexerlib.Transaction, error)
+	ReadTransaction(hash string) (*indexerlib.Transaction, error)
+	ReadBlocks(options *postgresdriver.ReadBlocksOptions) ([]*indexerlib.Block, error)
+	ReadBlock(hash string) (*indexerlib.Block, error)
 }
 
 // Resolver struct handler for dependency injection to GraphQL operations
 type Resolver struct {
-	Driver driver
+	Reader reader
 }
