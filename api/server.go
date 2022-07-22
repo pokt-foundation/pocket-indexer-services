7167db8d158dce3bb186332b66e8821dba644347
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-22 13:20:31 -0400

    chore: activate grapqhl playground

--- api/server.go
+++ api/server.go
@@ -29,7 +29,8 @@ func main() {
 		Reader: driver,
 	}}))
 
-	runPlayground := flag.Bool("p", false, "Flag to activate playground")
+	// TODO: Change to false
+	runPlayground := flag.Bool("p", true, "Flag to activate playground")
 
 	flag.Parse()
 
